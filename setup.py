469677c4ca02a9dca8bf5841eba0accd8f9e8a68
moroen <moroen@gmail.com>
2020-01-23 21:54:02 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="tradfricoap",
+    version="0.0.2",
+    url="https://github.com/moroen/tradfricoap.git",
+    author="moroen",
+    author_email="moroen@gmail.com",
+    description="Controlling IKEA-Tradfri from the commmand line",
+    packages=["tradfricoap"],
+    include_package_data=True,
+    setup_requires=["cython"],
+    install_requires=["cython"],
+    scripts=["tradfri"],
+)
---
+++ tradfricoap/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.2"
+
+class Error(Exception):
+    pass
+
+class IllegalMethodError(Exception):
+    pass
+
+class ApiNotFoundError(Error):
+    def __init__(self, api, message):
+        self.message = message
+        self.api = api
