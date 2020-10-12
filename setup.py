9164a4872d454f65ec496a3f4b9aff3b6eb4b861
moroen <moroen@gmail.com>
2020-10-12 17:34:54 +0200

    Added readme to description

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="tradfricoap",
     version="0.0.5",
@@ -7,6 +13,8 @@ setup(
     author="moroen",
     author_email="moroen@gmail.com",
     description="Controlling IKEA-Tradfri",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=["tradfricoap"],
     include_package_data=True,
     setup_requires=[],
