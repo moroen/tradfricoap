97ce9bfb5e9edcae299719c55d4ca5c64a250cc4
moroen <moroen@gmail.com>
2020-12-16 22:11:22 +0100

    Bump version: 0.0.16 → 0.0.17

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="tradfricoap",
-    version="0.0.16",
+    version="0.0.17",
     url="https://github.com/moroen/tradfricoap.git",
     author="moroen",
     author_email="moroen@gmail.com",
